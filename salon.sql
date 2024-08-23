ae4ab24f4e10d1f2b87f07ea8d740f2da1e057a4
Facundo Fernández <ffernanndez97@gmail.com>
2024-08-22 23:16:08 -0300

    Update salon.sql

--- salon.sql
+++ salon.sql
@@ -1,1 +1,19 @@
+psql --username=freecodecamp --dbname=postgres
 
+CREATE DATABASE salon;
+
+\c salon;
+
+CREATE TABLE customers (customer_id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, phone VARCHAR(50) UNIQUE NOT NULL);
+
+CREATE TABLE appointments(appointment_id SERIAL PRIMARY KEY NOT NULL, time VARCHAR(50) UNIQUE NOT NULL, customer_id INT, service_id INT);
+
+CREATE TABLE  services(service_id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL); 
+
+ALTER TABLE appointments 
+  ADD CONSTRAINT appointment_service FOREIGN KEY(service_id) services(service_id);  
+ALTER TABLE appointments 
+  ADD CONSTRAINT appointment_customer FOREIGN KEY(customer_id) customers(customer_id);
+
+
+  
