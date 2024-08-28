56caa76f426dad0502c540dad741e0c4389903f7
Facundo Fernández <ffernanndez97@gmail.com>
2024-08-27 23:30:51 -0300

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,3 +1,14 @@
+#! /bin/bash
+
+#TYPE on bash terminal to grant permission
+#chmod +x salon.sh
+#./ salon.sh
+#--------
+
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+echo -e "\n ----- Barbershop -----\n"
+
+
 echo -e "\nPlease select your service:"
 
 MAIN_MENU() {
@@ -18,31 +29,42 @@ done
 
 #------ SERVICE SELECTION 
 
-read SERVICE_SELECTION
-SERVICE_EXIST=$($PSQL "SELECT service_id FROM services WHERE service_id=$SERVICE_SELECTION")
+read SERVICE_ID_SELECTED
+SERVICE_EXIST=$($PSQL "SELECT service_id FROM services WHERE service_id=$SERVICE_ID_SELECTED")
 
 if [[ -z $SERVICE_EXIST ]]
 then 
   MAIN_MENU "Couldn't find that service. Please choose again!"
 fi
 
-#------ NUMBER INSERTION
+# READ PHONE NUMBER
 
 echo -e "\nInsert phone number please: "
-read PHONE_NUMBER
 
-CUSTOMER_EXIST=$($PSQL "SELECT customer_id name FROM customers WHERE phone=$PHONE_NUMBER")
+read CUSTOMER_PHONE
+CUSTOMER_EXIST=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
 
 if [[ -z $CUSTOMER_EXIST ]]
   then 
   echo -e "New here? Insert your name please"
-  read NAME
-  INSERTED=$($PSQL "INSERT INTO customers (name, phone) VALUES ('$NAME', '$PHONE_NUMBER')")
-  echo -e "Thanks $NAME! Please pick the time for your appointment:"
-  #29--------------------------------------------------------------------------------
+  read CUSTOMER_NAME
+  INSERTED=$($PSQL "INSERT INTO customers (name, phone) VALUES ('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+  echo -e "Thanks $CUSTOMER_NAME! Please pick the time for your appointment:"
 fi
+
+#----- SERVICE NAME
+
+SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id='$SERVICE_ID_SELECTED'")
+
+#---- READ TIME
+read SERVICE_TIME
+echo -e "\nI have put you down for a '$SERVICE_NAME' at '$SERVICE_TIME', '$CUSTOMER_NAME'."
+
+
 }
 
- MAIN_MENU 
+MAIN_MENU
+
+ ------------------------------------------
 
  
